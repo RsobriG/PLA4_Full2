63edbbbbb8bf80e2735d4efd3ea48edb1bb169f2
Roger <rsobri@gmail.com>
2019-06-23 01:42:04 +0200

    Finalització de l'exercici 8 i millora de la mostra del resultat al html

--- PLA4_Full2/WebContent/Exercici8.js
+++ PLA4_Full2/WebContent/Exercici8.js
@@ -3,40 +3,47 @@
  de la primera columna d'acord a la següent formula: (a1+....+an )/n
  */
 
-var matriu = [1,1,0,1,1,
+var matriu = [1,1,3,1,3,
 			  2,2,0,2,2,
 			  4,4,4,4,0,
-			  7,7,7,7,7,
-			  9,9,9,9,9];
+			  7,4,7,7,7,
+			  9,9,9,9,5];
 var sumatori = 0;
-var resultatMitjanaArit = [];
+var resultatMitjanaArit = [0,0,0,0,0];
 
 
 function mitjanaArit(matriu){
-	for (var i=0; i<24; i++){
+	for (var i=0; i<=24; i++){
+		sumatori = 0;
 		if ((i==0)||(i==5)||(i==10)||(i==15)||(i==20)){
+			sumatori = resultatMitjanaArit[0];
 			sumatori += matriu[i];
 			resultatMitjanaArit[0] = sumatori;
 		}
 		if ((i==1)||(i==6)||(i==11)||(i==16)||(i==21)){
+			sumatori = resultatMitjanaArit[1];
 			sumatori += matriu[i];
 			resultatMitjanaArit[1] = sumatori;
 		}
 		if ((i==2)||(i==7)||(i==12)||(i==17)||(i==22)){
+			sumatori = resultatMitjanaArit[2];
 			sumatori += matriu[i];
 			resultatMitjanaArit[2] = sumatori;
 		}
 		if ((i==3)||(i==8)||(i==13)||(i==18)||(i==23)){
+			sumatori = resultatMitjanaArit[3];
 			sumatori += matriu[i];
 			resultatMitjanaArit[3] = sumatori;
 		}
 		if ((i==4)||(i==9)||(i==14)||(i==19)||(i==24)){
+			sumatori = resultatMitjanaArit[4];
 			sumatori += matriu[i];
-			resultatMitjanaArit[3] = sumatori;
+			resultatMitjanaArit[4] = sumatori;
 		}
 	}
 	
-	mitjanaArit = resultatMitjanaArit[0] ;
+	mitjanaArit = resultatMitjanaArit;
+	
 	
 	return mitjanaArit;
 }
