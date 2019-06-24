246fdba356b9bfc6dd9de397f9ba9705d4aac60b
Roger <rsobri@gmail.com>
2019-06-24 13:31:59 +0200

    Comentaris afegits

--- PLA4_Full2/WebContent/Exercici6.js
+++ PLA4_Full2/WebContent/Exercici6.js
@@ -12,21 +12,26 @@ var llistaFinal = [1,1,1,1,1,
 var matriunxn = [];
 var tamanyMatriunxn = prompt("Indica el tamany de la matriu (min 2x2, max 5x5): ", "");
 
-
+// Funció per sumar la diagonal de la matriu escollida per l'usuari
 function sumaDiagonalMatriunxn (matriunxn){
-	if (tamanyMatriunxn == 2){
-		matriunxn = [llistaFinal[0],llistaFinal[1],llistaFinal[5],llistaFinal[6]];
-		sumaDiagonalMatriunxn =  matriunxn[0]+matriunxn[3];	
+	if (tamanyMatriunxn == 2){ //condicional que s'hi entra en cas que l'usuari vulgui una matriu de 2x2
+		matriunxn = [llistaFinal[0],llistaFinal[1],llistaFinal[5],llistaFinal[6]]; //es crea una matriu de 2x2 amb tots els valors de la matriu extrets de la llistaFinal
+		sumaDiagonalMatriunxn =  matriunxn[0]+matriunxn[3];	 //es sumen els valors corresponents de la diagonal de la matriu 2x2
 	}
-	if (tamanyMatriunxn == 3){
+	if (tamanyMatriunxn == 3){ //condicional que s'hi entra en cas que l'usuari vulgui una matriu de 3x3
+		//es crea una matriu de 3x3 amb tots els valors de la matriu extrets de la llistaFinal
 		matriunxn = [llistaFinal[0],llistaFinal[1],llistaFinal[2],llistaFinal[5],llistaFinal[6],llistaFinal[7],llistaFinal[10],llistaFinal[11],llistaFinal[12]];
+		//es sumen els valors corresponents de la diagonal de la matriu 3x3
 		sumaDiagonalMatriunxn =  matriunxn[0]+matriunxn[4]+matriunxn[8];	
 	}
-	if (tamanyMatriunxn == 4){
+	if (tamanyMatriunxn == 4){ //condicional que s'hi entra en cas que l'usuari vulgui una matriu de 4x4
+		// es crea una matriu de 4x4 amb tots els valors de la matriu extrets de la llistaFinal
 		matriunxn = [llistaFinal[0],llistaFinal[1],llistaFinal[2],llistaFinal[3],llistaFinal[5],llistaFinal[6],llistaFinal[7],llistaFinal[8],llistaFinal[10],llistaFinal[11],llistaFinal[12],llistaFinal[13],llistaFinal[15],llistaFinal[16],llistaFinal[17],llistaFinal[18]];
+		//es sumen els valors corresponents de la diagonal de la matriu 4x4
 		sumaDiagonalMatriunxn =  matriunxn[0]+matriunxn[5]+matriunxn[10]+matriunxn[15];	
 	}
-	if (tamanyMatriunxn == 5){
+	if (tamanyMatriunxn == 5){ //condicional que s'hi entra en cas que l'usuari vulgui una matriu de 5x5
+		//es sumen els valors de la matriu de 5x5 que és l amateixa matriu que la llitsaFinal
 		sumaDiagonalMatriunxn = llistaFinal[0]+llistaFinal[6]+llistaFinal[12]+llistaFinal[18]+llistaFinal[24]; 	
 	}
 		
