99b3ec2d87fecb49ce8ef93303ffee09de2d9215
Roger <rsobri@gmail.com>
2019-06-24 13:07:17 +0200

    Comentaris afegits

--- PLA4_Full2/WebContent/Exercici2.js
+++ PLA4_Full2/WebContent/Exercici2.js
@@ -9,13 +9,14 @@ var contadorCaracteres = 0;
 var ABC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var caractersRepetits = [];
 
-function contarCaracter(cadena){
-	for (var i=0; i<ABC.length; i++){
-		contadorCaracters = 0;
-		for (var j=0; j<cadena.length; j++){
-			if (cadena[j] == ABC[i]){
-				contadorCaracters++;
-				caractersRepetits[i] = "<BR>"+"La lletra "+ABC[i]+" esta repetida "+contadorCaracters+ " vegades"+ "<BR>";
+// funció per comptar els caràcters repetits de la cadena
+function contarCaracter(cadena){				
+	for (var i=0; i<ABC.length; i++){				//Bucle per recórrer tot l'abecedari (constant ABC)
+		contadorCaracters = 0;						// reiniciem la variable contadorCaracters cada vegada que volem mirar un caràcter de la cadena
+		for (var j=0; j<cadena.length; j++){		// Bucle per recórrer cada caràcter de la cadena i compararlo amb cada caràcter de l'abecedari
+			if (cadena[j] == ABC[i]){				// condicional que hi entrem en cas de que la lletra de la constant de l'abecedari sigui igual a la lletra de la cadena
+				contadorCaracters++;				// sumem un digit a contador caràcter
+				caractersRepetits[i] = "<BR>"+"La lletra "+ABC[i]+" esta repetida "+contadorCaracters+ " vegades"+ "<BR>";	//enviem a fora de la funció un text amb la lletra del abecedari trobada dins la cadena i indiquem les vegades que s'ha repetit dintre de la cadena
 			}
 		}
 	}
