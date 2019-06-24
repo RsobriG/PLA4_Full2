44bc9c1fc159c88da705bf856e7e838e97b3abed
Roger <rsobri@gmail.com>
2019-06-24 12:55:32 +0200

    Insertats comentaris

--- PLA4_Full2/WebContent/Exercici1.js
+++ PLA4_Full2/WebContent/Exercici1.js
@@ -7,12 +7,13 @@
 var numUsuari = prompt("Introduir un numero del 0 al 9: " , "");
 var contadorNum = 0;
 
+// Es crea funció per comptar els numeros més grans de l'escollit per l'usuari
 function contarNum (cadena){
-	for (var i=0; i<cadena.length; i++){
-		if (cadena[i] > numUsuari){
-			contadorNum = cadena.length - i;
-			break;
+	for (var i=0; i<cadena.length; i++){ 		//S'utilitza el bucle for per recórrer tota la cadena
+		if (cadena[i] > numUsuari){				// si es troba el numero indicat per l'usuari a la cadena s'entra a la condició
+			contadorNum = cadena.length - i;	// al entrar a la condició es resta la longitud de la cadena menys el últim numero trobat a la cadena igual al insertat per l'usuari
+			break;								// al entrar a la condició fem un break per sortir-hi
 		}
 	}
-	return contadorNum;
+	return contadorNum;							
 }
